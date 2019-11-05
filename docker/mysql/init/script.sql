62afda0707f1e55eae523bef7bb5ab9e365dee08
GUILDAM <bruno.heidorn@hotmail.com>
2019-11-04 22:29:35 -0300

    Ajustes

--- docker/mysql/init/script.sql
+++ docker/mysql/init/script.sql
@@ -3,7 +3,7 @@ CREATE database IF NOT EXISTS product;
 CREATE database IF NOT EXISTS wishlist;
 CREATE database IF NOT EXISTS cart;
 # create root user and grant rights;
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
+#CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
 GRANT ALL ON *.* TO 'root'@'%';
 CREATE USER 'springuser'@'%' IDENTIFIED BY 'timao';
 GRANT ALL ON product.* TO 'springuser'@'%';
--- proxy/src/main/java/com/amazonia/proxy/Application.java
+++ proxy/src/main/java/com/amazonia/proxy/Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 public class Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
