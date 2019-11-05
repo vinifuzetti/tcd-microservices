850857e2e7a99628fcdcec1b19985927f1716e87
GUILDAM <bruno.heidorn@hotmail.com>
2019-11-04 23:26:35 -0300

    Ajustado rotas
    amazonia/*

--- cart/src/main/java/com/amazonia/cart/CartController.java
+++ cart/src/main/java/com/amazonia/cart/CartController.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @RestController
-@RequestMapping(path="/cart")
 public class CartController {
 	
 	@Autowired // This means to get the bean called userRepository
